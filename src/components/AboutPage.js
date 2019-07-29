c3d4f3ce8ec97945ec5f93ec9aa57e6bb0e8c11a
Jonathan <jonathanferre2@gmail.com>
2019-07-29 20:42:20 -0300

    Creating React Components

---
+++ src/components/AboutPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <React.Fragment>
+      <h2>About</h2>
+      <p>This app uses React.</p>
+    </React.Fragment>
+  );
+}
+// class AboutPage extends React.Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <h2>About</h2>
+//         <p>This app uses React.</p>
+//       </React.Fragment>
+//     );
+
+//   }
+// }
+
+export default AboutPage;
