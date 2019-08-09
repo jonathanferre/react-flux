5c1a5fb9610921ad0d094d08cf59818104f7c405
Jonathan <jonathanferre2@gmail.com>
2019-08-09 11:12:34 -0300

    Forms - reusableInput, save data via API, validation, read URL to populated form

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -8,16 +8,5 @@ function AboutPage() {
     </React.Fragment>
   );
 }
-// class AboutPage extends React.Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <h2>About</h2>
-//         <p>This app uses React.</p>
-//       </React.Fragment>
-//     );
-
-//   }
-// }
 
 export default AboutPage;
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,12 +1,59 @@
-import React from "react";
-import { Prompt } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
+import { toast } from "react-toastify";
 
 const ManageCoursePage = props => {
+  const [errors, setErrors] = useState({});
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: ""
+  });
+
+  useEffect(() => {
+    const slug = props.match.params.slug; // from the path '/courses/:slug'
+    if (slug) {
+      courseApi.getCourseBySlug(slug).then(_course => setCourse(_course));
+    }
+  }, [props.match.params.slug]);
+
+  function formIsValid() {
+    const _errors = {};
+    if (!course.title) _errors.title = "Title is required";
+    if (!course.authorId) _errors.authorId = "Author ID is required";
+    if (!course.category) _errors.category = "Category is required";
+
+    setErrors(_errors);
+    //Form is valid if the error object has no properties
+    return Object.keys(_errors).length === 0;
+  }
+
+  function handleChange(event) {
+    const updateCourse = { ...course, [event.target.name]: event.target.value };
+    setCourse(updateCourse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!formIsValid()) return;
+    courseApi.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast.success("Course saved.");
+    });
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
-      <Prompt when={true} message="Are you sure you want to leave?" />
-      {props.match.params.slug}
+      <CourseForm
+        errors={errors}
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </>
   );
 };
