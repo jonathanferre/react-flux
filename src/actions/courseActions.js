c072b24f71bc07c40834af2d9c84e7ac3ba063f2
Jonathan <jonathanferre2@gmail.com>
2019-08-14 22:35:39 -0300

    Flux-p1 Dispatcher, actions, store: private Storage

---
+++ src/actions/courseActions.js
@@ -0,0 +1,14 @@
+import dispatcher from "../appDispatcher";
+import * as courseApi from "../api/courseApi";
+import actionTypes from "./actionTypes";
+
+//Action creator
+export function saveCourse(course) {
+  return courseApi.saveCourse(course).then(savedCourse => {
+    dispatcher.dispatch({
+      //Action
+      actionType: actionTypes.CREATE_COURSE,
+      course: savedCourse
+    });
+  });
+}
