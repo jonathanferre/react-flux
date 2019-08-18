6e9bf7e42d559ef1b0e8dfc120187cf0e0b8718a
Jonathan <jonathanferre2@gmail.com>
2019-08-18 15:45:51 -0300

    Flux-p2 load list from API to store

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import CourseForm from "./CourseForm";
-import * as courseApi from "../api/courseApi";
+import courseStore from "../stores/courseStore";
+//import * as courseApi from "../api/courseApi";
 import { toast } from "react-toastify";
+import * as courseActions from "../actions/courseActions";
 
 const ManageCoursePage = props => {
   const [errors, setErrors] = useState({});
@@ -16,9 +18,14 @@ const ManageCoursePage = props => {
   useEffect(() => {
     const slug = props.match.params.slug; // from the path '/courses/:slug'
     if (slug) {
-      courseApi.getCourseBySlug(slug).then(_course => setCourse(_course));
+      setCourse(courseStore.getCourseBySlug(slug));
     }
   }, [props.match.params.slug]);
+
+  function handleChange(event) {
+    const updateCourse = { ...course, [event.target.name]: event.target.value };
+    setCourse(updateCourse);
+  }
 
   function formIsValid() {
     const _errors = {};
@@ -31,15 +38,10 @@ const ManageCoursePage = props => {
     return Object.keys(_errors).length === 0;
   }
 
-  function handleChange(event) {
-    const updateCourse = { ...course, [event.target.name]: event.target.value };
-    setCourse(updateCourse);
-  }
-
   function handleSubmit(event) {
     event.preventDefault();
     if (!formIsValid()) return;
-    courseApi.saveCourse(course).then(() => {
+    courseActions.saveCourse(course).then(() => {
       props.history.push("/courses");
       toast.success("Course saved.");
     });
